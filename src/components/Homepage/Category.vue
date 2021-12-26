e99d1556ae05b3f67ab41d03f21c4b771067246f
BohdanFiliuk <filiukbohdan123@gmail.com>
2021-12-26 17:57:21 +0100

    style mobile homepage categories section

---
+++ src/components/Homepage/Category.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="category">
+    <img
+      v-if="product === 'headphones'"
+      src="../../assets/shared/desktop/image-category-thumbnail-headphones.png"
+      alt=""
+      class="category__thumbnail"
+    />
+    <img
+      v-if="product === 'speakers'"
+      src="../../assets/shared/desktop/image-category-thumbnail-speakers.png"
+      alt=""
+      class="category__thumbnail"
+    />
+    <img
+      v-if="product === 'earphones'"
+      src="../../assets/shared/desktop/image-category-thumbnail-earphones.png"
+      alt=""
+      class="category__thumbnail"
+    />
+    <h2 class="category__name">{{ product }}</h2>
+    <router-link class="category__link" to="#">
+      Shop
+      <img
+        src="../../assets/shared/desktop/icon-arrow-right.svg"
+        alt=""
+        class="category__link__icon"
+      />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: {
+    product: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.category {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f1f1f1;
+  margin-bottom: 6.8rem;
+  border-radius: 0.8rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &__thumbnail {
+    width: 15rem;
+    position: relative;
+    top: -5.2rem;
+    margin-bottom: -5.2rem;
+  }
+
+  &__name {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 2.049rem;
+    letter-spacing: 0.107rem;
+  }
+
+  &__link {
+    margin-top: 1.7rem;
+    margin-bottom: 2.2rem;
+    color: #797979;
+    font-size: 1.3rem;
+    font-weight: 700;
+    line-height: 1.776rem;
+    letter-spacing: 0.1rem;
+    text-transform: uppercase;
+    display: flex;
+    align-items: center;
+
+    &__icon {
+      margin-left: 1.332rem;
+    }
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,26 @@
 <template>
   <Header />
+  <main>
+    <section class="categories">
+      <Category product="headphones" />
+      <Category product="speakers" />
+      <Category product="earphones" />
+    </section>
+  </main>
 </template>
 
 <script>
 import Header from "../components/Homepage/Header.vue";
+import Category from "../components/Homepage/Category";
 
 export default {
   name: "Home",
-  components: { Header },
+  components: { Header, Category },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.categories {
+  padding: 9.2rem 2.4rem 0 2.4rem;
+}
+</style>
