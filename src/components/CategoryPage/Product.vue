9a2ff91367108cdc0e17c59903c91bf8949cb91a
BohdanFiliuk <filiukbohdan123@gmail.com>
2022-01-05 17:22:53 +0100

    display product images in category page

---
+++ src/components/CategoryPage/Product.vue
@@ -0,0 +1,132 @@
+<template>
+  <article class="product">
+    <img :src="editSrc" class="product__preview" />
+    <div class="product__text">
+      <p class="product__text__tag" v-if="product && product.new">
+        New product
+      </p>
+      <h2 class="product__text__title">{{ product && product.name }}</h2>
+      <p class="product__text__description">
+        {{ product && product.description }}
+      </p>
+      <button class="product__text__btn default-btn">See product</button>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: { product: Object },
+  computed: {
+    editSrc() {
+      return require(`../../${this.product.image.desktop.slice(2)}`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.product {
+  margin: 6rem auto;
+  width: 32.7rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    width: 68.9rem;
+  }
+
+  @media (min-width: 1205px) {
+    width: 111rem;
+    flex-direction: row;
+
+    &:nth-child(odd) {
+      flex-direction: row-reverse;
+
+      & .product__text {
+        margin-left: 0;
+        margin-right: 12.5rem;
+      }
+    }
+  }
+
+  & * {
+    text-align: center;
+  }
+
+  &:first-child {
+    margin-top: 6.4rem;
+  }
+
+  &__preview {
+    width: 100%;
+    border-radius: 0.8rem;
+    object-fit: cover;
+    object-position: center;
+
+    @media (min-width: 768px) {
+      height: 35.2rem;
+    }
+
+    @media (min-width: 1205px) {
+      width: 54rem;
+      height: 56rem;
+    }
+  }
+
+  &__text {
+    margin-top: 3.2rem;
+
+    @media (min-width: 768px) {
+      width: 57.2rem;
+    }
+
+    @media (min-width: 1205px) {
+      margin: 0;
+      margin-left: 12.5rem;
+      width: 44.5rem;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      * {
+        text-align: left;
+      }
+    }
+
+    &__tag {
+      font-size: 1.4rem;
+      font-weight: 400;
+      line-height: 1.912rem;
+      letter-spacing: 1rem;
+      text-transform: uppercase;
+      color: rgba(216, 125, 74, 1);
+    }
+
+    &__title {
+      margin: 2.4rem 0;
+      font-weight: 700;
+      font-size: 2.8rem;
+      line-height: 3.825rem;
+      letter-spacing: 0.1rem;
+      text-transform: uppercase;
+
+      @media (min-width: 768px) {
+        font-size: 4rem;
+        line-height: 4.4rem;
+        letter-spacing: 0.143rem;
+      }
+    }
+
+    &__description {
+      margin-bottom: 2.4rem;
+      font-weight: 500;
+      font-size: 1.5rem;
+      line-height: 2.5rem;
+      color: #8d8d8d;
+    }
+  }
+}
+</style>
--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -1,14 +1,58 @@
 <template>
-  <Header :category="$route.params.category" />
+  <Header :category="category" />
+  <Product
+    v-for="product in sortedProducts"
+    :key="product.id"
+    :product="product"
+  />
   <ProductPageNavigation />
 </template>
 
 <script>
 import ProductPageNavigation from "../components/ProductPageNavigation.vue";
 import Header from "../components/CategoryPage/Header.vue";
+import Product from "../components/CategoryPage/Product.vue";
+import jsonData from "../data.json";
+
 export default {
   name: "CategoryPage",
-  components: { ProductPageNavigation, Header },
+  components: { ProductPageNavigation, Header, Product },
+  data() {
+    return {
+      productInfo: jsonData,
+      category: this.$route.params.category,
+    };
+  },
+  computed: {
+    headphones() {
+      return this.productInfo.filter(
+        (product) => product.category === "headphones"
+      );
+    },
+    earphones() {
+      return this.productInfo.filter(
+        (product) => product.category === "earphones"
+      );
+    },
+    speakers() {
+      return this.productInfo.filter(
+        (product) => product.category === "speakers"
+      );
+    },
+    currentProducts() {
+      return this.category === "headphones"
+        ? this.headphones
+        : this.category === "earphones"
+        ? this.earphones
+        : this.speakers;
+    },
+    sortedProducts() {
+      return this.currentProducts.slice().sort((a, b) => b.new - a.new);
+    },
+  },
+  beforeMount() {
+    console.log("productInfo", this.currentProducts);
+  },
 };
 </script>
 
